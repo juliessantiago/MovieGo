b2fa5629d82feac5c4bad8576e94162f4d1a9e29
Julie <Santiago.sjulie@gmail.com>
2021-01-14 20:04:06 -0300

    Cria função de exibição de filmes mais recentes e altera edição

--- js/script.js
+++ js/script.js
@@ -13,36 +13,80 @@ var firebaseConfig = {
 
   const dbRef = firebase.database().ref('moviego');
 
-/*===========================Edita dados============================== */ 
-/*Funções de edição e exclusão de dados definidas antes de listar porque são chamadas 
-no onclick do innerHtml da função de exibição*/ 
-function editarDado(){
-    try{
-        alert ('Dado alterado'); 
-    }catch(error){
-        alert ('Não foi possível editar o dado' + error); 
-    }
-   
-}
-/*========================Lista Dados===================================*/ 
-let lista = document.getElementById('lista'); 
-let dados = ''; 
-try{
-    let db = dbRef.on('child_added', function(snapshot){
-        let titulo = (snapshot.val().titulo_filme); 
-        let ano = (snapshot.val().ano_filme); 
-        let dir = (snapshot.val().diretor_filme); 
-        let fat = (snapshot.val().faturamento_filme); 
-     dados = '<tr><td>'+titulo+'</td><td>'+ano+'</td><td>'+dir+ 
-     '<td>'+fat+'<td><button onclick="editarDado()" class="btn btn-secondary">Editar</button></td><td><button class="btn btn-secondary" id="excluir">Excluir</button></td></tr>'+dados; 
-        lista.innerHTML = dados; 
-     })
-}catch(error){
-    console.log('Não foi possível exibir lista de dados'+error); 
+/*========================Lista Todos os dados do banco===================================*/ 
+document.onload = exibeLista(); 
+
+function exibeLista(){
+        let lista = document.getElementById('lista'); 
+        let dados = ''; 
+        try{
+            dbRef.on('child_added', function(snapshot){ 
+                let id = snapshot.key; 
+                let titulo = snapshot.val().titulo_filme; 
+                let ano = snapshot.val().ano_filme; 
+                let dir = snapshot.val().diretor_filme; 
+                let fat = snapshot.val().faturamento_filme; 
+            dados = '<tr><td id ="altTitulo">'+titulo+'</td><td>'+ano+'</td><td>'+dir+ 
+            '<td>'+fat+'<td><button id="botaoEditar" onclick = "exibeFormEdita(\''+id+'\')" class="btn btn-secondary">Editar</button></td><td><button class="btn btn-secondary" onclick = "excluiDado(\''+id+'\')">Excluir</button></td></tr>'+dados; 
+                lista.innerHTML = dados; //Essa não é a forma mais correta de inserir o elemento button e passar o parâmetro pra função
+            })
+        }catch(error){
+            console.log('Não foi possível exibir lista de dados'+error); 
+        }
 }
 
+/*============================Exclui Dado===================================*/
+function excluiDado(id){
+    console.log('Chamou função excluir')
+        try{
+            let  ref = firebase.database().ref('moviego/'+id); 
+            ref.remove()
+            alert('Dado excluído com sucesso'); 
+            exibeLista(); 
+            exibeRecentes()
+            
+        }catch(error){
+            alert('Não foi possível excluir o dado '+error); 
+        }
+}
+/*=========================Edita dado específico============================*/
+let formularioEdicao = document.getElementById('formEdita')
+formularioEdicao.style.display = 'none'
+
+function exibeFormEdita(id){
+    formularioEdicao.style.display = 'block'
+    let inputId = document.getElementById('idEdita'); 
+    inputId.value = id; 
+}
+
+function editaDado() {
+    console.log('Função Edita foi chamada')
+    var id = document.getElementById('idEdita').value; 
+    var titulo = document.getElementById('tituloEdita').value; 
+    var ano = document.getElementById('anoEdita').value; 
+    var diretor = document.getElementById('diretorEdita').value; 
+    var faturamento = document.getElementById('faturamentoEdita').value; 
+
+    try {
+        let alteraDado = firebase.database().ref('moviego').child(id); 
+        alteraDado.update({
+                titulo_filme : titulo, 
+                ano_filme : ano, 
+                diretor_filme: diretor, 
+                faturamento_filme : faturamento  
+        })  
+        alert ('Dado alterado com sucesso')
+        formularioEdicao.style.display = 'none'
+        exibeLista(); 
+        exibeRecentes()
+
+    }catch(error){
+        alert('Não foi possível alterar dado'+ error)
+    }
+}
 /*==========================Insert========================================*/
 document.getElementById('inserir').onclick = function(){
+    
     var titulo = document.getElementById('titulo').value; 
     var ano = document.getElementById('ano').value; 
     var diretor = document.getElementById('diretor').value; 
@@ -50,22 +94,34 @@ document.getElementById('inserir').onclick = function(){
     
     try {
         dbRef.push({
+               
                 titulo_filme : titulo, 
                 ano_filme : ano, 
                 diretor_filme: diretor, 
                 faturamento_filme : faturamento  
     }); 
-    let formulario = document.getElementById('formFilmes'); 
-    formulario.reset(); 
-    alert('Dados inseridos com sucesso'); 
+        let formulario = document.getElementById('formFilmes'); 
+        formulario.reset(); //Limpa campos do formulário 
+        alert('Dados inseridos com sucesso'); 
 
-
+        exibeRecentes()
     }catch(error){
         alert('Não foi possível inserir'+ error)
     }
     
 } 
- //Editar: 
-
-
+/*=====================Mostra os mais recentes===========================*/
+document.onload = exibeRecentes(); 
+function exibeRecentes(){
+    let listaRecentes = document.getElementById('listaRecentes'); 
+    let dadosRecentes = ''; 
+    try{
+        dbRef.orderByChild("ano_filme").limitToFirst(5).on("child_added", function(snapshot){ 
+            dadosRecentes = '<tr><td>'+snapshot.val().titulo_filme+'</td><td>'+snapshot.val().ano_filme+'</td></tr>'+dadosRecentes; 
+                listaRecentes.innerHTML = dadosRecentes;
+        })
+    }catch(error){
+        console.log('Não foi possível exibir lista de dados'+error); 
+    }
+}
 
